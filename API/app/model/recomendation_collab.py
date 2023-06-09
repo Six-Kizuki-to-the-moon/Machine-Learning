68aa3280632d79a5a4a732b036af125534fb9e4c
Dewa Putra Hernanda <dewananda089@gmail.com>
2023-06-10 00:37:34 +0700

    Update recomendation_collab.py

--- API/app/model/recomendation_collab.py
+++ API/app/model/recomendation_collab.py
@@ -6,10 +6,10 @@ from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split 
 from tensorflow.keras.models import Model 
 
-def recomendation(destination, ratings, user_id, detail_user):
+def recomendation(destination, ratings, user_id, user_location = "", np_val = 5):
     # pre processing dataset
-    number_user = len(ratings['User_Id'].unique())
-    number_destination = len(ratings['Place_Id'].unique())
+    number_user = len(ratings['user_wisata'].unique())
+    number_destination = len(ratings['wisata_id'].unique())
     train, test = train_test_split(ratings, test_size = 0.2)
     
     EMBEDDING_DIM = 50
@@ -38,18 +38,18 @@ def recomendation(destination, ratings, user_id, detail_user):
     early_stopping = EarlyStopping(patience=5, restore_best_weights=True)
     model.compile(optimizer=Adam(learning_rate = 0.0005), loss='mean_squared_error') 
     
-    history = model.fit(x= [train.Place_Id, train.User_Id], 
-                    y= train.Place_Ratings, 
-                    validation_data = ([test.Place_Id, test.User_Id], test.Place_Ratings), 
-                    epochs =100,
+    history = model.fit(x= [train.wisata_id, train.user_wisata], 
+                    y= train.wisata_rating, 
+                    validation_data = ([train.wisata_id, train.user_wisata], train.wisata_rating), 
+                    epochs =25,
                     callbacks=[early_stopping],
                     verbose=0)
     
     # recommendation system function using collaborative filtering
-    def collaborative_rec(User_Id, destination ,model, np_val, detail_user):
+    def collaborative_rec(User_Id, destination ,model, np_val, user_location = '', ):
         # detail user : digunakan untuk menyimpan data detail user untuk mengerucutkan data yang akan di outputkan
     
-        if User_Id in ratings['User_Id'].values:
+        if User_Id in ratings['user_wisata'].values:
                 destination = destination.copy()
                 user_ids = np.array([User_Id] * len(destination))
                 results = model([destination.Place_Id.values, user_ids]).numpy().reshape(-1)
@@ -58,14 +58,14 @@ def recomendation(destination, ratings, user_id, detail_user):
                 destination = destination.sort_values('predicted_rating', ascending = False)
         else:
                 destination = destination.copy()
-                destination = destination.sort_values('Rating', ascending = False)
+                destination = destination.sort_values('rating', ascending = False)
 
-        if detail_user != None:
-            destination = destination[destination['City'] == detail_user]
+        if user_location != 'None':
+            destination = destination[destination['city'] == user_location]
 
         dataFinal = destination[:np_val]
-        return dataFinal['Place_Id'].tolist()
+        return dataFinal[['name_wisata', 'city', 'destination_photo','description_wisata']].to_dict(orient='records')
     
-    recommendations = collaborative_rec(user_id, destination, model, 5, detail_user)
+    recommendations = collaborative_rec(user_id, destination, model, np_val, user_location)
     
     return recommendations
