72bcb8d127fde4767936ff6d9966a23d26ed5fdd
Dewa Putra Hernanda <dewananda089@gmail.com>
2023-06-11 12:22:20 +0700

    update optimal recomendation_collab

--- API/app/model/recomendation_collab.py
+++ API/app/model/recomendation_collab.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 from tensorflow.keras.layers import Input, Embedding, Dot, Flatten, Dropout
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping
@@ -10,43 +11,21 @@ from tensorflow.keras.models import Model
 from math import radians
 
 def recomendation(destination, ratings, user_id, user_lat, user_long, np_val = 5):
-    # pre processing dataset
-    number_user = len(ratings['user_wisata'].unique())
-    number_destination = len(ratings['wisata_id'].unique())
+    ratings = ratings.sort_values('wisata_rating', ascending=False).drop_duplicates(subset=['wisata_id', 'user_wisata'], keep='first')
+    
     train, test = train_test_split(ratings, test_size = 0.2)
     
-    EMBEDDING_DIM = 50
-
-    # input layers 
-    place_input = Input(shape=[1])
-    user_input = Input(shape=[1])
-
-    # embedding layers add dropout 
-    place_embedding = Embedding(number_destination+1 , EMBEDDING_DIM)(place_input)
-    place_embedding = Dropout(0.2)(place_embedding)
-
-    user_embedding = Embedding(number_user+1 , EMBEDDING_DIM)(user_input)
-    user_embedding = Dropout(0.2)(user_embedding)
-
-    # flatten the embedddings
-    place_flat = Flatten()(place_embedding)
-    user_flat = Flatten()(user_embedding)
-
-    # output layer
-    output = Dot(1)([place_flat, user_flat])
-
-    # the model
-    model = Model([place_input, user_input], [output])
+    # load model pre trained
+    model =  tf.keras.models.load_model('API\ml-dev\pretrained_collab_rec')
     
     early_stopping = EarlyStopping(patience=5, restore_best_weights=True)
     model.compile(optimizer=Adam(learning_rate = 0.0005), loss='mean_squared_error') 
     
-    history = model.fit(x= [train.wisata_id, train.user_wisata], 
+    model.fit(x= [train.wisata_id, train.user_wisata], 
                     y= train.wisata_rating, 
                     validation_data = ([train.wisata_id, train.user_wisata], train.wisata_rating), 
-                    epochs =18,
-                    callbacks=[early_stopping],
-                    verbose=0)
+                    epochs =5,
+                    callbacks=[early_stopping])
     
     def haversine_distance(lat1, long1, lat2, long2):
         earth_radius = 6371  # Radius of the Earth in kilometers (source: google)
@@ -66,35 +45,33 @@ def recomendation(destination, ratings, user_id, user_lat, user_long, np_val = 5
         destination = destination[destination['distance'] <= max_distance]
         destination = destination.drop('distance', axis=1)
         return destination
-
-    def filter_by_city(destination):
-        city_filter = ['Jakarta', 'Bandung', 'Yogyakarta', 'Semarang', 'Surabaya']
-        destination = destination[destination['city'].isin(city_filter)]
-        return destination
-
+    
     # recommendation system function using collaborative filtering
-    def collaborative_rec(User_Id, destination ,model, np_val, user_lat, user_long ):
-        # detail user : digunakan untuk menyimpan data detail user untuk mengerucutkan data yang akan di outputkan
+    def collaborative_rec(User_Id, destination ,model, u_lat, u_long, np_val = 6 ):
+        """
+            User_Id : untuk melakukan rekomendasi berdasarkan User_Id yang diberikan
+            destinatin : dataframe dengan format column terdapat "user_wisata, wisata_id, wisata_rating, name_wisata, category, description_wisata, destination_lat, destination_long, destination_photo, city"
+            model : model hasil yang sudah di training sebelumnya
+            np_val : banyaknya output yang akan diberikan
+            u_lat & u_long : berguna untuk menentukan destinasi wisata terdekat dari lat & long yang diberikan
+        """
     
         if User_Id in ratings['user_wisata'].values:
-                destination = destination.copy()
-                user_ids = np.array([User_Id] * len(destination))
-                results = model([destination.Place_Id.values, user_ids]).numpy().reshape(-1)
+            destination = destination.copy()
+            user_ids = np.array([User_Id] * len(destination))
+            results = model([destination.id.values, user_ids]).numpy().reshape(-1)
 
-                destination['predicted_rating'] = pd.Series(results)
-                destination = destination.sort_values('predicted_rating', ascending = False)
+            destination['predicted_rating'] = pd.Series(results)
+            destination = filter_by_location(destination, u_lat, u_long, 30)[:20]
+            destination = destination.sort_values('predicted_rating', ascending = False)
         else:
-                destination = destination.copy()
-                destination = destination.sort_values('rating', ascending = False)
-
-        # if user_location != 'None':
-        #     destination = destination[destination['city'] == user_location]
-
-        dataFinal = filter_by_location(destination, user_lat, user_long, 30)
+            destination = destination.copy()
+            destination = filter_by_location(destination, u_lat, u_long, 30)[:20]
+            destination = destination.sort_values('rating', ascending = False)  
+            
+        return destination[['name_wisata', 'city','coordinate', 'destination_photo','description_wisata']][:np_val].to_dict(orient='records')
     
-        dataFinal = destination[:np_val]
-        return dataFinal[['name_wisata', 'city', 'destination_photo','description_wisata']].to_dict(orient='records')
     
-    recommendations = collaborative_rec(user_id, destination, model, np_val, user_lat, user_long)
+    recommendations = collaborative_rec(int(user_id), destination, model, user_lat, user_long, np_val)
     
     return recommendations
